70f147f334a21347799696e216f296994f718ba7
Alekhaya Datta <55008888+alekhaya99@users.noreply.github.com>
2021-05-30 22:25:29 +0800

    Add files via upload

---
+++ BarChart/Horizantal Bar Chart.R
@@ -0,0 +1,7 @@
+library("ggplot2")
+
+df_used_car<-read.csv("D:/Data Science/365Data Science/Data VIs/Bar Chart/bar_chart_data.csv",header=TRUE,sep=",")
+
+bar_chart<-ggplot(df_used_car,aes(y=Brand,x=Cars.Listings))+geom_bar(stat="identity",width = 0.8,color='navy',fill='navy')+ggtitle("Car Listings by Brand")+theme_classic()+theme(axis.text.y =element_text(angle = 45,hjust = 1) )+labs(x="Number of Listings",y=NULL)
+
+bar_chart
